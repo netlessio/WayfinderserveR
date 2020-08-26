dfa22df678379c752243d6db72ec40ae85f1930b
netlessio <109853277+netlessio@users.noreply.github.com>
2020-08-26 14:01:01 +0000

    Fix #159

--- modules/Common/org/jtotus/common/DateIterator.java
+++ modules/Common/org/jtotus/common/DateIterator.java
@@ -3,4 +3,9 @@ This file is part of jTotus.
 
 jTotus is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Fre
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+jTotus is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR
