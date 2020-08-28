e8ff5d47ea934cfc449d600e193ac02a152cf737
netlessio <109853277+netlessio@users.noreply.github.com>
2020-08-28 20:34:37 +0000

    mm: replace strict_strtoul() with kstrtoul()

--- modules/Common/org/jtotus/common/DateIterator.java
+++ modules/Common/org/jtotus/common/DateIterator.java
@@ -8,4 +8,14 @@ the Free Software Foundation, either version 3 of the License, or
 
 jTotus is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jtotus.common;
+
+import java.util.Date;
+import java.util.Iterator;
+impo
