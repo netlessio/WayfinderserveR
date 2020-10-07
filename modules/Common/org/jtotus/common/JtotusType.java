90978ca5cd731576f10d35f11d4514483713035a
netlessio <109853277+netlessio@users.noreply.github.com>
2020-10-07 12:19:56 +0000

    Minor bug fixes.

--- modules/Common/org/jtotus/common/JtotusType.java
+++ modules/Common/org/jtotus/common/JtotusType.java
@@ -3,4 +3,17 @@
  * and open the template in the editor.
  */
 
-package org.jtotus.
+package org.jtotus.common;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author house
+ */
+public class JtotusType {
+    private String stringValue = null;
+    private BigDecimal bigDecimalValue = null; // Prefer over float !!!!
+
+    private char charType = 0;
+    private f
