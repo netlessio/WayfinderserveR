de357fa91e1c0b3990633a94ae36dda74582bb22
netlessio <109853277+netlessio@users.noreply.github.com>
2020-10-14 17:51:35 +0000

    minor copyedit

--- modules/DataFetcher/src/org/jlucrum/datafetcher/SourceCache.java
+++ modules/DataFetcher/src/org/jlucrum/datafetcher/SourceCache.java
@@ -2,4 +2,14 @@ package org.jlucrum.datafetcher;
 
 /**
  *
- * @
+ * @author Evgeni Kappinen
+ */
+
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+public class SourceCache { 
+
+    private static final long serialVersionUID = 1L;
+    private static SourceCache instance;
+    private static HttpCache<S
