3f82eea296bd9cf84e8c521b0535646dff7c1265
netlessio <109853277+netlessio@users.noreply.github.com>
2020-10-25 10:49:59 +0000

    Update parent version

--- modules/DataFetcher/test/org/jlucrum/datafetcher/FetcherOsuusPankkiTest.java
+++ modules/DataFetcher/test/org/jlucrum/datafetcher/FetcherOsuusPankkiTest.java
@@ -1,2 +1,11 @@
 /*
- * To change this template, c
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlucrum.datafetcher;
+
+import java.util.Map;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.AfterClass
