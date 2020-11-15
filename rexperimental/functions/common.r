3643a024028b19f3dbabbc1ed05fa4afdf358b12
netlessio <109853277+netlessio@users.noreply.github.com>
2020-11-15 20:27:22 +0000

    Semantics

--- rexperimental/functions/common.r
+++ rexperimental/functions/common.r
@@ -1,4 +1,10 @@
 #Converts Map to time series object
 jluc.convMapToTs <- function(map) {
   
-  simpleMap 
+  simpleMap <- .jstrVal(map)
+  simpleMap2<-gsub("^\\{", "", simpleMap)
+  simpleMap3<-gsub("}$", "", simpleMap2)
+  dayAndValue<-unlist(strsplit(simpleMap3, split=","))
+  retTimeSeries<-NULL
+  for (i in dayAndValue) {
+    line<-unlist(strsplit(i,spli
