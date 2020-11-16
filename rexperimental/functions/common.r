f50a2654eddabc8e4710d356f32b517445b6c596
netlessio <109853277+netlessio@users.noreply.github.com>
2020-11-16 08:56:50 +0000

    visual updates

--- rexperimental/functions/common.r
+++ rexperimental/functions/common.r
@@ -7,4 +7,15 @@ jluc.convMapToTs <- function(map) {
   dayAndValue<-unlist(strsplit(simpleMap3, split=","))
   retTimeSeries<-NULL
   for (i in dayAndValue) {
-    line<-unlist(strsplit(i,spli
+    line<-unlist(strsplit(i,split="="))
+    newvalue<-xts(as.double(line[2]), as.Date(line[1]))
+    retTimeSeries<-rbind(retTimeSeries, newvalue)
+  }
+  
+  return(retTimeSeries)
+
+#  data <- data.frame(row.names=c("date", "value"))
+
+#  entrySet <- map$entrySet()
+#  iter<-entrySet$iterator()
+#  whi
