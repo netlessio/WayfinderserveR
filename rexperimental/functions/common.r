4c6364911cf59d73e9c61522808037cd2a0700b3
netlessio <109853277+netlessio@users.noreply.github.com>
2020-11-17 11:25:42 +0000

    Minified

--- rexperimental/functions/common.r
+++ rexperimental/functions/common.r
@@ -18,4 +18,11 @@ jluc.convMapToTs <- function(map) {
 
 #  entrySet <- map$entrySet()
 #  iter<-entrySet$iterator()
-#  whi
+#  while(.jsimplify(iter$hasNext())) {
+#    nextEntry <- iter$"next"()
+#    dayData<-data.frame(date=as.Date(nextEntry$getKey()), 
+#                     value=nextEntry$getValue());
+#    data <- rbind(data,dayData);
+#  }
+#  tsdata <- xts(data[,2], data[,1])
+#  re
