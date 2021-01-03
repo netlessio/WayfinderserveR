60677fa5ed33dbcc975d2721a3e0d1a10098a068
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-03 16:50:58 +0000

    improved javadoc

--- rexperimental/memory.r
+++ rexperimental/memory.r
@@ -9,4 +9,10 @@
     obj.class <- napply(names, function(x) as.character(class(x))[1])
     obj.mode <- napply(names, mode)
     obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
-    obj.size <- napply(nam
+    obj.size <- napply(names, object.size)
+    obj.dim <- t(napply(names, function(x)
+                        as.numeric(dim(x))[1:2]))
+    vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+    obj.dim[vec, 1] <- napply(names, length)[vec]
+    out <- data.frame(obj.type, obj.size, obj.dim)
+    name
