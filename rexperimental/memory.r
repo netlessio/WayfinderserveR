46c45334393a2f975ffe956f623742bed984edaa
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-03 06:53:03 +0000

    C++

--- rexperimental/memory.r
+++ rexperimental/memory.r
@@ -1,4 +1,8 @@
 #Refrence: http://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
 
 # improved list of objects
-.ls.objects <
+.ls.objects <- function (pos = 1, pattern, order.by,
+                        decreasing=FALSE, head=FALSE, n=5) {
+    napply <- function(names, fn) sapply(names, function(x)
+                                         fn(get(x, pos = pos)))
+ 
