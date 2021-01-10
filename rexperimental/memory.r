790efd2a431f3186228c453e9c4ee4ca1a1b365c
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-10 16:39:07 +0000

    goodbye

--- rexperimental/memory.r
+++ rexperimental/memory.r
@@ -24,4 +24,7 @@
 }
 # shorthand
 lsos <- function(..., n=10) {
-    .ls.objects(..., order.by="Size
+    .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+}
+
+# lsos(pattern="*")
---
+++ rexperimental/preload.r
@@ -0,0 +1,4 @@
+rluc.preload <- function(luc.pathToLibs, luc.pathToClasses, workingDir) {
+  
+#luc.pathToLibs <- paste("/home/home/Dropbox/jlucrum/lib");
+#luc.pathToClasses <- paste("/home/home/NetBeansProjects/JLucrum/b
