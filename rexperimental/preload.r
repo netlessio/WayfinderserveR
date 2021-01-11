ce64b1e5093f833ca0af9d79ae5568de62239a71
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-11 14:20:15 +0000

    merge in jb-mr0-release history after reset to jb-dev

--- rexperimental/preload.r
+++ rexperimental/preload.r
@@ -1,4 +1,20 @@
 rluc.preload <- function(luc.pathToLibs, luc.pathToClasses, workingDir) {
   
 #luc.pathToLibs <- paste("/home/home/Dropbox/jlucrum/lib");
-#luc.pathToClasses <- paste("/home/home/NetBeansProjects/JLucrum/b
+#luc.pathToClasses <- paste("/home/home/NetBeansProjects/JLucrum/build/classes");
+
+Sys.setenv(TZ="GMT")
+
+#Set working path
+#setwd("~/Dropbox/jlucrum/rengine");
+setwd(workingDir);
+
+library(rJava);
+library(tseries);
+library(quantmod);
+library(gtools)
+require(ggplot2)
+require(glmulti)
+library(TTR)
+library(MASS)
+#library(fGarch
