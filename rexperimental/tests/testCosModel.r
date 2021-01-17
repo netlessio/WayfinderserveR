b787305b8db646e529d41d8d3a47870accdc9974
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-17 09:12:26 +0000

    install script

--- rexperimental/tests/testCosModel.r
+++ rexperimental/tests/testCosModel.r
@@ -1,2 +1,11 @@
 jluc.testModel1 <- function(asset="TESTcompany",
-             
+                            fromDate=as.Date(Sys.Date()-150),
+                            toDate=Sys.Date(), 
+                            lag=1)
+{
+  
+  start<-as.double(difftime(toDate, fromDate, unit="days"))
+  
+  new.data<-NULL
+  for (i in start:0) {
+    new.dat
