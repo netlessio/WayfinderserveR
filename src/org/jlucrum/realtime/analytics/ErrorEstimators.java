7a1801acc631eba5efd1664f5291b18a6f9664c8
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-24 10:43:24 +0000

    Updated todo list

--- src/org/jlucrum/realtime/analytics/ErrorEstimators.java
+++ src/org/jlucrum/realtime/analytics/ErrorEstimators.java
@@ -67,3 +67,65 @@ public class ErrorEstimators {
      * Theil Statistics
      *
      * @param target the value to be predicted of the time series
+     * @param output model output (prediction)
+     * @return error
+     */
+    public static double theil(double[] target, double[] output) {
+        double sum = 0;
+        double sum2 = 0;
+        double size;
+
+        if (target.length < output.length) {
+            size = target.length;
+        } else {
+            size = output.length;
+        }
+
+        if (size < 1) {
+            throw new RuntimeException("Array is to small for calculation error");
+        }
+
+        for (int i = 1; i < size; i++) {
+            sum += Math.pow(target[i - 1] - output[i - 1], 2);
+            sum2 += Math.pow(target[i - 1] - target[i], 2);
+        }
+
+        return sum / sum2;
+    }
+
+
+    /**
+     * POCID (Prediction of Forecast the Alterations of Direction)
+     *
+     * @param target the value to be predicted of the time series
+     * @param output model output (prediction)
+     * @return error
+     */
+    public static double pocid(double[] target, double[] output) {
+        double sum = 0;
+        double size;
+
+        if (target.length < output.length) {
+            size = target.length;
+        } else {
+            size = output.length;
+        }
+
+        if (size < 1) {
+            throw new RuntimeException("Array is to small for calculation error");
+        }
+
+        for (int i = 1; i < size; i++) {
+            double d = (target[i] - target[i - 1]) * (output[i] - output[i - 1]);
+            if (d > 0) {
+                d = 1;
+            } else {
+                d = 0;
+            }
+            sum += d;
+        }
+
+        return (100 * sum) / (size - 1);
+    }
+
+}
