a3a39c178460fb47233e36e7875417069f15944b
netlessio <109853277+netlessio@users.noreply.github.com>
2021-01-24 11:16:22 +0000

    s

---
+++ src/org/jlucrum/realtime/eventtypes/MarketData.java
@@ -0,0 +1,10 @@
+
+package org.jlucrum.realtime.eventtypes;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import org.joda.time.DateTime;
+
+/**
