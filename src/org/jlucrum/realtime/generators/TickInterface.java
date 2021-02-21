3dc3388abccf2650ad7884efff5076f2f4224b51
netlessio <109853277+netlessio@users.noreply.github.com>
2021-02-21 07:32:48 +0000

    video: mdss: LiveDisplay driver

--- src/org/jlucrum/realtime/generators/TickInterface.java
+++ src/org/jlucrum/realtime/generators/TickInterface.java
@@ -1,3 +1,17 @@
 /*
  * To change this template, choose Tools | Templates
- * and open the t
+ * and open the template in the editor.
+ */
+
+package org.jlucrum.realtime.generators;
+
+import com.espertech.esper.client.UpdateListener;
+
+/**
+ *
+ * @author house
+ */
+public interface TickInterface extends UpdateListener {
+
+    public boolean subscribeForTicks();
+    p
