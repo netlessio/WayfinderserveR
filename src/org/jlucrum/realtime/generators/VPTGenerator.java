2083f63e251944b07ecee102dd6b7d2d88aa5d5c
netlessio <109853277+netlessio@users.noreply.github.com>
2021-02-27 07:43:35 +0000

    Compile.

--- src/org/jlucrum/realtime/generators/VPTGenerator.java
+++ src/org/jlucrum/realtime/generators/VPTGenerator.java
@@ -1,4 +1,10 @@
 /*
     This file is part of jTotus.
 
-    jTotus is free software: you can redistribute it and/
+    jTotus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jTotus is distributed in the hope that it will be useful,
+    but
