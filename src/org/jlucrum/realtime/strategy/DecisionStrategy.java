8cd6141ac206519858ac0d89bb0cefe9c1ac138f
netlessio <109853277+netlessio@users.noreply.github.com>
2021-03-20 10:58:48 +0000

    change API

--- src/org/jlucrum/realtime/strategy/DecisionStrategy.java
+++ src/org/jlucrum/realtime/strategy/DecisionStrategy.java
@@ -6,4 +6,10 @@ import org.jtotus.common.MethodResults;
 import java.util.HashMap;
 
 /**
- * This file is part of J
+ * This file is part of JTotus.
+ * <p/>
+ * jTotus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ 
