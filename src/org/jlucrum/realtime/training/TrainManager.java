f1dd12876b5b07aebd17a9a693b328145fdb411b
netlessio <109853277+netlessio@users.noreply.github.com>
2021-03-21 11:10:47 +0000

    Initial entry.

--- src/org/jlucrum/realtime/training/TrainManager.java
+++ src/org/jlucrum/realtime/training/TrainManager.java
@@ -1,3 +1,8 @@
 package org.jlucrum.realtime.training;
 
-import org.jluc
+import org.jlucrum.realtime.BrokerWatcher;
+import org.jlucrum.realtime.eventtypes.MarketData;
+import org.jlucrum.realtime.eventtypes.MarketSignal;
+import org.jlucrum.realtime.strategy.DecisionStrategy;
+import org.jlucrum.realtime.strategy.SimpleIndicatorsOnlyStrategy;
+import co
