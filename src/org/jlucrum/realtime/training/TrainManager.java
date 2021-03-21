fbb1904f051d4087655dc2fa7ef44f1ad7454748
netlessio <109853277+netlessio@users.noreply.github.com>
2021-03-21 16:37:34 +0000

    cpufreq: Avoid using global variable total_cpus

--- src/org/jlucrum/realtime/training/TrainManager.java
+++ src/org/jlucrum/realtime/training/TrainManager.java
@@ -5,4 +5,10 @@ import org.jlucrum.realtime.eventtypes.MarketData;
 import org.jlucrum.realtime.eventtypes.MarketSignal;
 import org.jlucrum.realtime.strategy.DecisionStrategy;
 import org.jlucrum.realtime.strategy.SimpleIndicatorsOnlyStrategy;
-import co
+import com.espertech.esper.client.EPRuntime;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jtotus.common.MethodResults;
+import org.jtotus.config.ConfPortfolio;
+import org.jtotus.config.ConfTrainWi
