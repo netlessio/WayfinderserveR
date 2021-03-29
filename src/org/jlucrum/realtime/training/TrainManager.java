80fdd472d513b00687c1c03c4e83d23157ece633
netlessio <109853277+netlessio@users.noreply.github.com>
2021-03-29 11:54:22 +0000

    Update ff_old_new.sh

--- src/org/jlucrum/realtime/training/TrainManager.java
+++ src/org/jlucrum/realtime/training/TrainManager.java
@@ -38,4 +38,12 @@ import org.jtotus.threads.MethodFuture;
  * <p/>
  * jTotus is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+*
