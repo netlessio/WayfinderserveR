1a6e524f8cda6ea3082f02d6ce3fd870bca3f2b0
netlessio <109853277+netlessio@users.noreply.github.com>
2021-05-09 17:24:40 +0000

    Entities

--- src/org/jtotus/config/ConfTaLibEMA.java
+++ src/org/jtotus/config/ConfTaLibEMA.java
@@ -9,4 +9,16 @@
     jTotus is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU G
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.jtotus.config;
+
+/**
+ *
+ * @author Evgeni Kappinen
+ */
+p
