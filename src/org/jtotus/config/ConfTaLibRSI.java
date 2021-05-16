524d6ab559c6a38873f0e4de676885c4ab316597
netlessio <109853277+netlessio@users.noreply.github.com>
2021-05-16 23:11:55 +0000

    add source

--- src/org/jtotus/config/ConfTaLibRSI.java
+++ src/org/jtotus/config/ConfTaLibRSI.java
@@ -7,4 +7,9 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 jTotus is distributed in the hope that it will be useful,
-but W
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with jTotus.  If not, see <http:/
