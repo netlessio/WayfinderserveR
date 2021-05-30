7254bdfe112fe2e9ddb5fb758b5b9f4aa72e70b8
netlessio <109853277+netlessio@users.noreply.github.com>
2021-05-30 09:07:10 +0000

    Clean translations

--- src/org/jtotus/crypt/JtotusKeyRingPassword.java
+++ src/org/jtotus/crypt/JtotusKeyRingPassword.java
@@ -11,4 +11,18 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Pu
+You should have received a copy of the GNU General Public License
+along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jtotus.crypt;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Evgeni Kappinen
+ */
+public class JtotusKeyRingPassword {
+
+    private String keyRingPassword = n
