8de00c71c6c90731e94f0f4194f2abce3628dce4
netlessio <109853277+netlessio@users.noreply.github.com>
2021-06-06 21:01:38 +0000

    Refactor packages

--- src/org/jtotus/database/AutoUpdateStocks.java
+++ src/org/jtotus/database/AutoUpdateStocks.java
@@ -1,2 +1,9 @@
 /*
-This file is part of jT
+This file is part of jTotus.
+
+jTotus is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+jTotus i
