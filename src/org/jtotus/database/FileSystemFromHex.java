4d0342d0916c53cae73fb9291f5b57cf32f125c6
netlessio <109853277+netlessio@users.noreply.github.com>
2021-08-29 15:16:25 +0000

    fix query

--- src/org/jtotus/database/FileSystemFromHex.java
+++ src/org/jtotus/database/FileSystemFromHex.java
@@ -2,4 +2,7 @@
  * JStock - Free Stock Market Software
  * Copyright (C) 2009 Yan Cheng CHEOK <yccheok@yahoo.com>
  *
- * This prog
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later vers
