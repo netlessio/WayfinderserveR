887bfd10190dd0a6083fa8078544119ff57c8006
netlessio <109853277+netlessio@users.noreply.github.com>
2021-11-06 08:26:29 +0000

    1er commit

--- src/org/jtotus/database/InterfaceDataBase.java
+++ src/org/jtotus/database/InterfaceDataBase.java
@@ -3,4 +3,8 @@
  * Copyright (C) 2009 Yan Cheng CHEOK <yccheok@yahoo.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be use
