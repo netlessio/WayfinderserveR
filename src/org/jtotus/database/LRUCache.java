fe3392b3d0221644f956aa2854191ef245f07e01
netlessio <109853277+netlessio@users.noreply.github.com>
2021-11-14 22:49:46 +0000

    Adding photography portfolio.

--- src/org/jtotus/database/LRUCache.java
+++ src/org/jtotus/database/LRUCache.java
@@ -11,4 +11,12 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a 
+You should have received a copy of the GNU General Public License
+along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jtotus.database;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.jod
