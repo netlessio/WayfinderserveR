c6e910727d3830024646d25cf7d736fee5550b6b
netlessio <109853277+netlessio@users.noreply.github.com>
2021-11-21 17:32:22 +0000

    simple test

--- src/org/jtotus/database/LRUCache.java
+++ src/org/jtotus/database/LRUCache.java
@@ -43,4 +43,12 @@ public class LRUCache implements Cache {
     private class LruCache<A, B> extends LinkedHashMap<A, B> {
         private final int maxEntries;
 
-        public LruCache(final int max
+        public LruCache(final int maxEntries) {
+            super(maxEntries + 1, 1.0f, true);
+            this.maxEntries = maxEntries;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(final Map.Entry<A, B> eldest) {
+            return super.size() > maxEntries;
+     
