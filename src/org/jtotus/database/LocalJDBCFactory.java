d1898c4f5ce03baac27ff298f6dc17b956c664a7
netlessio <109853277+netlessio@users.noreply.github.com>
2021-12-05 05:39:27 +0000

    intelli_plug: fix logic error for eco mode profiles

--- src/org/jtotus/database/LocalJDBCFactory.java
+++ src/org/jtotus/database/LocalJDBCFactory.java
@@ -9,4 +9,14 @@
     jTotus is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public Licens
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.jtotus.database;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import
