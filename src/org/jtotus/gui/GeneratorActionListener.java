da5aab05dc920cfc916273f0f6ecc2aedde375d2
netlessio <109853277+netlessio@users.noreply.github.com>
2022-03-25 22:00:40 +0000

    added gulpfile

--- src/org/jtotus/gui/GeneratorActionListener.java
+++ src/org/jtotus/gui/GeneratorActionListener.java
@@ -9,4 +9,16 @@ the Free Software Foundation, either version 3 of the License, or
 jTotus is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for m
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+ */
+
+package org.jtotus.gui;
+
+import org.jlucrum.realtime.generators.TickInterface;
+import jav
