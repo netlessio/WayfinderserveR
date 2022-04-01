7afb378ef433fd0b99b89e4d54f97003d8d7babd
netlessio <109853277+netlessio@users.noreply.github.com>
2022-04-01 08:26:10 +0000

    Added libs

--- src/org/jtotus/gui/JTotusMethodView.java
+++ src/org/jtotus/gui/JTotusMethodView.java
@@ -9,4 +9,12 @@ the Free Software Foundation, either version 3 of the License, or
 jTotus is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Publi
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jtotus.gui;
+
+import java.awt.event.ActionEvent;
+import j
