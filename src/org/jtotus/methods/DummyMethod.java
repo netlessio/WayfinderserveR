7246ae5fdd0db6cb7350fb57d0621871088fff46
netlessio <109853277+netlessio@users.noreply.github.com>
2022-10-09 19:05:00 +0000

    usb: hub: Use correct reset for wedged USB3 devices that are NOTATTACHED

--- src/org/jtotus/methods/DummyMethod.java
+++ src/org/jtotus/methods/DummyMethod.java
@@ -9,4 +9,15 @@
     jTotus is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public Li
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with jTotus.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+package org.jtotus.methods;
+
+import org.jlucrum.realtime.eventtypes.MarketData;
+import java.util.Random;
+im
