28ab7ac7b187e09806c252513c6839d1593160f6
netlessio <109853277+netlessio@users.noreply.github.com>
2022-10-11 20:07:11 +0000

    report changes

--- src/org/jtotus/methods/GroovyScipts.java
+++ src/org/jtotus/methods/GroovyScipts.java
@@ -1,2 +1,16 @@
 /*
- * To chan
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jtotus.methods;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author house
+ */
+public interface GroovyScipts {
+
+    public void loadScripts(LinkedList<MethodEntry
