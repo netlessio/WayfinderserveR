2b206f25cf36850a74a06e95006891ad93559025
netlessio <109853277+netlessio@users.noreply.github.com>
2022-10-30 19:27:07 +0000

    Fixup version numbers following release

--- src/org/jtotus/network/JtotusProxy.java
+++ src/org/jtotus/network/JtotusProxy.java
@@ -7,4 +7,17 @@
  * http://www.jguru.com/faq/view.jsp?EID=9920
  * http://stackoverflow.com/questions/1626549/authenticated-http-proxy-with-java
  */
-package or
+package org.jtotus.network;
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+
+/**
+ *
+ * @author Evgeni Kappinen
+ */
+public class JtotusProxy {
+
+    private String httpProxyURL = null;
+    private int httpProxyPort = 8080;
