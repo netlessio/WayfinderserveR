f7622887a17fbfdc38210731c8c7eb7390df6945
netlessio <109853277+netlessio@users.noreply.github.com>
2022-11-05 17:58:04 +0000

    Release 0.1.13

--- src/org/jtotus/network/NetworkTickConnector.java
+++ src/org/jtotus/network/NetworkTickConnector.java
@@ -1,2 +1,17 @@
 /*
- * To change this tem
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jtotus.network;
+
+import org.jlucrum.realtime.eventtypes.StockTick;
+
+/**
+ *
+ * @author house
+ */
+public interface NetworkTickConnector {
+    public boolean connect();
+
+    public StockTic
