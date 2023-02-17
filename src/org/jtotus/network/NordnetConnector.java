bdc33d2862227f3f4c809664778d407e4e5f15f6
netlessio <109853277+netlessio@users.noreply.github.com>
2023-02-17 09:18:27 +0000

    Added comment.

--- src/org/jtotus/network/NordnetConnector.java
+++ src/org/jtotus/network/NordnetConnector.java
@@ -8,4 +8,13 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org
