a3eb56f0f868691ba5c886da5c9731e6446489f2
netlessio <109853277+netlessio@users.noreply.github.com>
2023-02-17 08:25:39 +0000

    [maven-release-plugin] prepare release 0.1.1

--- src/org/jtotus/network/NordnetConnector.java
+++ src/org/jtotus/network/NordnetConnector.java
@@ -1,2 +1,11 @@
 /*
- * To change this tem
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jtotus.network;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator
