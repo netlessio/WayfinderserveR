af549fe3a3185a38fa654f5bd27d1f23829edb9f
netlessio <109853277+netlessio@users.noreply.github.com>
2023-03-19 08:26:27 +0000

    Drop dead code

--- src/org/jtotus/network/NordnetConnector.java
+++ src/org/jtotus/network/NordnetConnector.java
@@ -232,4 +232,8 @@ public class NordnetConnector implements BrokerConnector {
             HttpPost httpPost = new HttpPost(url);
 
             httpPost.addHeader("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.15) Gecko/20110303 Ubuntu/10.04 (lucid) Firefox/3.6.15");
-//            httpPost.addHeader("Referer"
+//            httpPost.addHeader("Referer", "");
+            
+            httpPost.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            httpPost.addHeader("Accept-Language", "en-us,en;q=0.5");
+            httpPost.addHeader("Keep-Alive", "11
