6d1a071fc7a99860564a8ab91a9ddd54e2d77275
netlessio <109853277+netlessio@users.noreply.github.com>
2023-03-26 11:27:35 +0000

    new lib

--- src/org/jtotus/network/StockType.java
+++ src/org/jtotus/network/StockType.java
@@ -2,4 +2,8 @@
     This file is part of jTotus.
 
     jTotus is free software: you can redistribute it and/or modify
-    it 
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jTotus is distributed in the hope that it will be usef
