a32b36d0bbf6d111492bbda51f209448ebf19ae0
netlessio <109853277+netlessio@users.noreply.github.com>
2023-04-12 19:37:57 +0000

    fix issue #8

--- test/org/jtotus/config/ConfSimpleTechnicalIndicatorsTest.java
+++ test/org/jtotus/config/ConfSimpleTechnicalIndicatorsTest.java
@@ -1,2 +1,11 @@
 /*
- * To c
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jtotus.config;
+
+import org.junit.Assert;
+import brokerwatcher.indicators.SimpleTechnicalIndicators;
+import org.junit.AfterClass;
+import org.junit.Be
