b7642a7dd407b467d07ab5311ab832ff862b454b
netlessio <109853277+netlessio@users.noreply.github.com>
2023-04-13 13:00:06 +0000

    Extra

--- test/org/jtotus/config/ConfSimpleTechnicalIndicatorsTest.java
+++ test/org/jtotus/config/ConfSimpleTechnicalIndicatorsTest.java
@@ -8,4 +8,20 @@ package org.jtotus.config;
 import org.junit.Assert;
 import brokerwatcher.indicators.SimpleTechnicalIndicators;
 import org.junit.AfterClass;
-import org.junit.Be
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author house
+ */
+public class ConfSimpleTechnicalIndicatorsTest {
+
+    public ConfSimpleTechnicalIndicatorsTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterCla
